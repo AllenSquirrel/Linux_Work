283f0d78db6fa79b59cb3a2b496485786dd6c363
dev <dev@localhost.localdomain>
2021-02-02 11:39:14 +0800

    test

---
+++ test.s
@@ -0,0 +1,39 @@
+	.file	"test.c"
+	.section	.rodata
+.LC0:
+	.string	"%d "
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	addl	$1, -4(%rbp)
+.L2:
+	cmpl	$9, -4(%rbp)
+	jle	.L3
+	movl	$10, %edi
+	call	putchar
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 5.3.1 20160406 (Red Hat 5.3.1-6)"
+	.section	.note.GNU-stack,"",@progbits
